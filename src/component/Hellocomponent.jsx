2a41749cd3d2edade387f3e71ecd6c28eb5bbd00
faizzaki14 <19104075@ittelkom-pwt.ac.id>
2022-01-05 17:39:01 +0700

    testing rect

---
+++ src/component/Hellocomponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Hellocomponent.css";
+
+const Hellocomponent = () => {
+  return (
+    <div>
+      <h1 className="text-h1">Hello World</h1>
+    </div>
+  );
+};
+
+export default Hellocomponent;
